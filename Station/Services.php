d68efa9b85233172bfbb0a059a522357688bd634
AnthorNet <anthor.net@gmail.com>
2018-10-12 10:15:20 +0200

    Force stations services update time

--- Station/Services.php
+++ Station/Services.php
@@ -13,31 +13,30 @@ class Services
     static public function handle($stationId, $message)
     {
         $currentStation = \EDSM_System_Station::getInstance($stationId);
-        
+
         if($currentStation->isValid())
         {
             $servicesLastUpdate = $currentStation->getServicesLastUpdate();
-            
+
             if(!is_null($servicesLastUpdate) && strtotime($message['timestamp']) < strtotime($servicesLastUpdate))
             {
                 \EDSM_Api_Logger::log('<span class="text-info">EDDN\Station\Services:</span>          <span class="text-danger">' . $currentStation->getName() . ' (#' . $currentStation->getId() . ') services too old (' . $message['timestamp'] . ').</span>');
                 return;
             }
-            
+
             if(array_key_exists('StationServices', $message))
             {
                 $stationsModel              = new \Models_Stations;
                 $stationsServicesModel      = new \Models_Stations_Services;
-                
+
                 $newServices                = array();
                 $nbUpdates                  = 0;
-                $updateLastUpdate           = false;
-                
+
                 // Create list of services with ID
                 foreach($message['StationServices'] AS $service)
                 {
                     $serviceId = Service::getFromFd($service);
-                    
+
                     if(is_null($serviceId))
                     {
                         \EDSM_Api_Logger_Alias::log('Alias\Station\Service #' . $currentStation->getId() . ':' . $service);
@@ -47,9 +46,9 @@ class Services
                         $newServices[] = $serviceId;
                     }
                 }
-                
+
                 $oldServices = $stationsServicesModel->getByRefStation($currentStation->getId());
-                
+
                 // Delete services not in the station anymore
                 if(!is_null($oldServices) && count($oldServices) > 0)
                 {
@@ -59,76 +58,84 @@ class Services
                         if(in_array($oldService['refService'], $newServices))
                         {
                             $nbUpdates++;
-                            
+
                             unset($newServices[array_search($oldService['refService'], $newServices)]);
                         }
                         else
                         {
                             \EDSM_Api_Logger::log('<span class="text-info">EDDN\Station\Services:</span>              - Delete ' . Service::get($oldService['refService']));
-                            
+
                             $stationsServicesModel->deleteById($oldService['id']);
-                            
+
                             // Delete commodities from the old market
                             if($oldService['refService'] == 1)
                             {
                                 $stationsCommoditiesModel = new \Models_Stations_Commodities;
                                 $stationsCommoditiesModel->deleteByRefStation($currentStation->getId());
-                                
+
                                 $stationsModel->updateById($currentStation->getId(), ['marketUpdateTime' => new \Zend_Db_Expr('NULL')]);
-                                
+
+                                // Tweet new oldest station if it's the oldest!
+                                $cacheKey               = sha1('\Process\Elite\Market::$oldestStationId');
+                                $bootstrap              = \Zend_Registry::get('Zend_Application');
+                                $cacheManager           = $bootstrap->getResource('cachemanager');
+                                $cacheFile              = $cacheManager->getCache('databaseFile');
+
+                                $isOldestStation        = $cacheFile->load($cacheKey);
+
+                                if($isOldestStation !== false && $isOldestStation == $currentStation->getId())
+                                {
+                                    \Process\Elite\Market::$sendTweet           = true;
+                                    \Process\Elite\Market::$sendCompleteStats   = false;
+                                    \Process\Elite\Market::run();
+                                }
+
                                 unset($stationsCommoditiesModel);
                             }
-                            
+
                             // Delete ships from the old shipyard
                             if($oldService['refService'] == 2)
                             {
                                 $stationsShipsModel = new \Models_Stations_Ships;
                                 $stationsShipsModel->deleteByRefStation($currentStation->getId());
-                                
+
                                 $stationsModel->updateById($currentStation->getId(), ['shipyardUpdateTime' => new \Zend_Db_Expr('NULL')]);
-                                
+
                                 unset($stationsShipsModel);
                             }
-                            
+
                             // Delete outfittings from the old outfitting
                             if($oldService['refService'] == 3)
                             {
                                 $stationsOutfittingsModel = new \Models_Stations_Outfittings;
                                 $stationsOutfittingsModel->deleteByRefStation($currentStation->getId());
-                                
+
                                 $stationsModel->updateById($currentStation->getId(), ['outfittingUpdateTime' => new \Zend_Db_Expr('NULL')]);
-                                
+
                                 unset($stationsOutfittingsModel);
                             }
-                            
-                            $updateLastUpdate = true;
                         }
                     }
                 }
-                
+
                 // Add new services
                 foreach($newServices AS $newService)
                 {
                     \EDSM_Api_Logger::log('<span class="text-info">EDDN\Station\Services:</span>              - Add ' . Service::get($newService));
-                    
+
                     $stationsServicesModel->insert(array(
                         'refStation'    => $currentStation->getId(),
                         'refService'    => $newService
                     ));
-                    
-                    $updateLastUpdate = true;
                 }
-                
+
                 if($nbUpdates > 0)
                 {
                     \EDSM_Api_Logger::log('<span class="text-info">EDDN\Station\Services:</span>          Updated ' . \Zend_Locale_Format::toNumber($nbUpdates) . ' services');
                 }
-                
-                if($updateLastUpdate === true)
-                {
-                    $stationsModel->updateById($currentStation->getId(), ['servicesUpdateTime' => $message['timestamp']]);
-                }
-                
+
+                $stationsModel->updateById($currentStation->getId(), ['servicesUpdateTime' => $message['timestamp']]);
+
                 unset($stationsModel, $stationsServicesModel);
             }
         }
